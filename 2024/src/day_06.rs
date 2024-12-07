eb1ef822b89395adff9ce470713bcef81328c605
Winston Liu <winstonliu@microsoft.com>
2024-12-06 21:03:27 -0500

    Day 6 part 2
    Well, it works...

--- 2024/src/day_06.rs
+++ 2024/src/day_06.rs
@@ -72,7 +72,103 @@ pub fn part_1(contents: &str) -> Result<u64, String> {
 }
 
 pub fn part_2(contents: &str) -> Result<u64, String> {
-    Ok(0)
+    let (mut grid, starting_position) = try_parse_grid(contents)?;
+
+    let mut possible_loops: u64 = 0;
+
+    // Awwwwww yeah it's O(n^(a lot)) time
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            let (mut row, mut col) = starting_position;
+            let mut direction = Direction::North;
+            let mut visited_positions = HashSet::from([(starting_position, direction)]);
+
+            let mut needs_reset = false;
+            if (i, j) != starting_position && grid[i][j] != Grid::Obstruction {
+                needs_reset = true;
+                grid[i][j] = Grid::Obstruction;
+            }
+
+            loop {
+                match direction {
+                    Direction::North => {
+                        if row == 0 {
+                            break;
+                        }
+
+                        if grid[row - 1][col] == Grid::Obstruction {
+                            direction = Direction::East;
+                            continue;
+                        }
+
+                        row -= 1;
+
+                        if !visited_positions.insert(((row, col), direction)) {
+                            possible_loops += 1;
+                            break;
+                        }
+                    }
+                    Direction::East => {
+                        if col == grid[row].len() - 1 {
+                            break;
+                        }
+
+                        if grid[row][col + 1] == Grid::Obstruction {
+                            direction = Direction::South;
+                            continue;
+                        }
+
+                        col += 1;
+
+                        if !visited_positions.insert(((row, col), direction)) {
+                            possible_loops += 1;
+                            break;
+                        }
+                    }
+                    Direction::South => {
+                        if row == grid.len() - 1 {
+                            break;
+                        }
+
+                        if grid[row + 1][col] == Grid::Obstruction {
+                            direction = Direction::West;
+                            continue;
+                        }
+
+                        row += 1;
+
+                        if !visited_positions.insert(((row, col), direction)) {
+                            possible_loops += 1;
+                            break;
+                        }
+                    }
+                    Direction::West => {
+                        if col == 0 {
+                            break;
+                        }
+
+                        if grid[row][col - 1] == Grid::Obstruction {
+                            direction = Direction::North;
+                            continue;
+                        }
+
+                        col -= 1;
+
+                        if !visited_positions.insert(((row, col), direction)) {
+                            possible_loops += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if needs_reset {
+                grid[i][j] = Grid::Path;
+            }
+        }
+    }
+
+    Ok(possible_loops)
 }
 
 fn try_parse_grid(contents: &str) -> Result<(Vec<Vec<Grid>>, Position), String> {
@@ -109,6 +205,7 @@ enum Grid {
     Obstruction,
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
     North,
     East,
