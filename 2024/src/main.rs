ffcbce6e10f9da1c0a8c8a52d2d87860843f8b9c
Winston Liu <winstonliu@microsoft.com>
2024-12-11 01:15:08 -0500

    Day 11 part 1
    And a naive implementation of part 2 that theoretically works on a memory-unconstrained machine

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -8,6 +8,7 @@ mod day_07;
 mod day_08;
 mod day_09;
 mod day_10;
+mod day_11;
 
 mod utilities;
 
@@ -38,6 +39,7 @@ fn main() -> Result<(), String> {
         [day_08::part_1, day_08::part_2],
         [day_09::part_1, day_09::part_2],
         [day_10::part_1, day_10::part_2],
+        [day_11::part_1, day_11::part_2],
     ];
 
     let days_to_run = match args.day {
