8d524026f3ded24e580e21c49f8d1805073c9555
Winston Liu <winstonliu@microsoft.com>
2024-12-24 16:51:43 -0500

    Format

--- 2024/src/day_11.rs
+++ 2024/src/day_11.rs
@@ -1,10 +1,12 @@
-
 pub fn part_1(contents: &str) -> Result<u64, String> {
     let mut stones = try_get_stones(contents)?;
     for _ in 0..25 {
         stones = stones.iter().flat_map(mutate_stone).collect();
     }
-    stones.len().try_into().map_err(|err: std::num::TryFromIntError| err.to_string())
+    stones
+        .len()
+        .try_into()
+        .map_err(|err: std::num::TryFromIntError| err.to_string())
 }
 
 pub fn part_2(contents: &str) -> Result<u64, String> {
@@ -12,13 +14,17 @@ pub fn part_2(contents: &str) -> Result<u64, String> {
     for _ in 0..75 {
         stones = stones.iter().flat_map(mutate_stone).collect();
     }
-    stones.len().try_into().map_err(|err: std::num::TryFromIntError| err.to_string())
+    stones
+        .len()
+        .try_into()
+        .map_err(|err: std::num::TryFromIntError| err.to_string())
 }
 
 fn try_get_stones(contents: &str) -> Result<Vec<u64>, String> {
-    contents.split_ascii_whitespace().map(|stone| {
-        stone.parse::<u64>().map_err(|err| err.to_string())
-    }).collect()
+    contents
+        .split_ascii_whitespace()
+        .map(|stone| stone.parse::<u64>().map_err(|err| err.to_string()))
+        .collect()
 }
 
 fn mutate_stone(stone: &u64) -> Vec<u64> {
@@ -28,7 +34,10 @@ fn mutate_stone(stone: &u64) -> Vec<u64> {
 
     let digits = stone.ilog10() + 1;
     if digits % 2 == 0 {
-        return vec![stone / (10_u64.pow(digits / 2)), stone % (10_u64.pow(digits / 2))];
+        return vec![
+            stone / (10_u64.pow(digits / 2)),
+            stone % (10_u64.pow(digits / 2)),
+        ];
     }
 
     vec![stone * 2024]
