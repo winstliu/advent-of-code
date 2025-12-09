6cc4ffe9cf60547306b9d6397a115a30a4cb0542
Winston Liu <Wliu1402@gmail.com>
2025-12-08 22:25:14 -0800

    Day 2 part 2
    The symmetry!!

--- 2025/src/day_02.rs
+++ 2025/src/day_02.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn part_1(contents: &str) -> Result<u64, String> {
     let ranges = contents.trim().split(',');
     let mut total: u64 = 0;
@@ -51,5 +53,52 @@ pub fn part_1(contents: &str) -> Result<u64, String> {
 }
 
 pub fn part_2(contents: &str) -> Result<u64, String> {
-    Ok(0)
+    let ranges = contents.trim().split(',');
+    let mut repetitive_ids = HashSet::new();
+    for range in ranges {
+        let (start, end) = range.split_once('-').ok_or("Range did not have a dash")?;
+        let start = start.parse::<u64>().map_err(|e| e.to_string())?;
+        let end = end.parse::<u64>().map_err(|e| e.to_string())?;
+
+        let start_digits = start.ilog10() + 1;
+        let end_digits = end.ilog10() + 1;
+
+        for digits in start_digits..=end_digits {
+            for digit_repetition in 1..=digits / 2 {
+                if digits % digit_repetition != 0 {
+                    continue;
+                }
+
+                let first_repeat = (0..digits)
+                    .rev()
+                    .step_by(digit_repetition as usize)
+                    .map(|i| 10_u64.pow(i))
+                    .sum::<u64>();
+
+                let diff_between_repeats = (0..digits)
+                    .step_by(digit_repetition as usize)
+                    .map(|i| 10_u64.pow(i))
+                    .sum::<u64>();
+
+                let first_repeat_within_range = if start < first_repeat {
+                    first_repeat
+                } else {
+                    first_repeat
+                        + (start - first_repeat).div_ceil(diff_between_repeats)
+                            * diff_between_repeats
+                };
+
+                let end = if end < 10_u64.pow(digits) {
+                    end
+                } else {
+                    10_u64.pow(digits) - 1
+                };
+
+                repetitive_ids.extend(
+                    (first_repeat_within_range..=end).step_by(diff_between_repeats as usize),
+                );
+            }
+        }
+    }
+    Ok(repetitive_ids.iter().sum())
 }
